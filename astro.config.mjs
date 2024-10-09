7c2f2bd68447206bf628ec322bc5f25d89d5c531
Fares Khaled <fares.khaled22@hotmail.com>
2024-10-09 20:08:04 +0100

    import vite-plugin-svgr pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
+import svgr from 'vite-plugin-svgr';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    esbuild: {
+      // Disable esbuild for JSX so Babel can take over
+      jsx: 'automatic',
+    },
+    plugins: [svgr()],
+  },
 });
